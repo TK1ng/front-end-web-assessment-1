8259dc90887c67d5f2e7b3a4a8f262371798784b
Taryn King <marshaeking@gmail.com>
2022-11-19 09:04:05 -0600

    add profile.js

---
+++ profile.js
@@ -0,0 +1,14 @@
+const favColorBtn = document.getElementById('color');
+const favPlaceBtn = document.getElementById('place');
+const favRitualBtn = document.getElementById('ritual');
+
+favColorBtn.addEventListener('click', () => {
+    alert('Purple 🟪: The color of royalty and magic 🪄');
+})
+
+favPlaceBtn.addEventListener('click', () => {
+    alert('On the coast in California 🌊');
+})
+favRitualBtn.addEventListener('click', () => {
+    alert('Morning yoga and burning sage 🧘🏾‍♂️✨');
+})
