dc125881a2eb052937f650587fe67879be7eef8b
Taryn King <marshaeking@gmail.com>
2022-11-20 21:58:28 -0600

    add websites to restaurant recommendations

--- neighborhood.js
+++ neighborhood.js
@@ -2,13 +2,14 @@ const getRecBtn = document.getElementById('get-rec');
 
 getRecBtn.addEventListener('click', getRecomendation);
 
+// Using objects to allow for inserting additional data to render with restaurant name
 const restaurants = [
-    { name: 'RH Rooftop', },
-    { name: 'Monarch', },
-    { name: 'Sixty Vines', },
-    { name: 'Ka Thai', },
-    { name: 'Vegan Food House', },
-    { name: 'Blue Sake Sushi', },
+    { name: 'RH Rooftop', website: `https://rh.com/dallas/restaurant/menu`, imgUrl: `` },
+    { name: 'Monarch', website: `https://www.monarchrestaurants.com/`, imgUrl: `` },
+    { name: 'Sixty Vines', website: ``, imgUrl: `` },
+    { name: 'Ka Thai', website: `https://www.kathairestaurant.com/`, imgUrl: `` },
+    { name: 'Vegan Food House', website: `https://veganfoodhouse.square.site/`, imgUrl: `` },
+    { name: 'Blue Sake Sushi', website: `https://bluesushisakegrill.com/`, imgUrl: `` },
 ]
 
 function getRecomendation() {
@@ -16,8 +17,15 @@ function getRecomendation() {
     recDiv.textContent = '';
 
     const randomIndex = Math.floor(Math.random() * 6);
-    const content = document.createTextNode(`${restaurants[randomIndex].name}`)
+    const rec = restaurants[randomIndex];
+    const link = document.createElement('a');
+
+    link.setAttribute('href', `${rec.website}`);
+    link.textContent = 'View website';
+
+    const content = document.createTextNode(`${rec.name} `)
 
     recDiv.appendChild(content);
+    recDiv.appendChild(link);
 }
 
